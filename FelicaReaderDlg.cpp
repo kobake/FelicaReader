e19801ae3dd2f70c5b7466ca8991b4ce6d66179d
kobake <kobake@user.sourceforge.net>
2015-01-11 23:32:07 +0900

    とりあえず Suica の識別子を読めるようになった

--- FelicaReaderDlg.cpp
+++ FelicaReaderDlg.cpp
@@ -113,10 +113,165 @@ HCURSOR CFelicaReaderDlg::OnQueryDragIcon()
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
+#include <string>
+#include <iostream>
+#include <sstream>
 
+#if 0
+#pragma comment(lib ,"felica_nfc_library.lib")
+#include "felica_nfc_library.h"
+// エラーコードの定義
+// ・FELICA_NFC_E_DRIVER_ERROR 0x80080000 ドライバでエラーが発生しました
+//
+// ドライバエラーコード
+// ・0x00000032 API の発行シーケンスが正しくありません。
+std::wstring GetFelicaErrorInfo()
+{
+	unsigned long e[2];
+	felicalib_nfc_get_last_error(e);
+	std::wostringstream ret;
+	char buf[64];
+	sprintf(buf, "0x%08X, 0x%08X", e[0], e[1]);
+	ret << buf;
+	//ret << e[0] << ", " << e[1];
+	return ret.str();
+}
+
+// 参考：http://codezine.jp/article/detail/7402?p=2
+std::wstring GetFelicaInfo()
+{
+	//std::wstringbuf retbuf;
+	//std::wostream ret(&retbuf);
+	std::wostringstream ret;//(&retbuf);
+
+	felicalib_nfc_start_logging("C:\\felica_test.log");
+	
+	// ライブラリの初期化
+	bool bSuccess;
+	bSuccess = felicalib_nfc_initialize();
+	if(!bSuccess){
+		ret << "initialize failure\r\n";
+		ret << "Error: " << GetFelicaErrorInfo() << "\r\n";
+		return ret.str();
+	}
+
+	// ポートのオープン
+	char szPort[16] = "USB0";
+	bSuccess = felicalib_nfc_open(szPort);
+	if(!bSuccess){
+		ret << "open failure\r\n";
+		ret << "Error: " << GetFelicaErrorInfo() << "\r\n";
+		return ret.str();
+	}
+	ret << "port = " << szPort << "\r\n"; //L"aa";//printf("port=%s\r\n", szPort);
+
+	// デバイスの捕捉とアクセス権の取得
+	DEVICE_INFO* pInfo = NULL;
+	bSuccess = felicalib_nfc_poll_and_start_dev_access(
+		DEVICE_TYPE_NFC_18092_212K    // FeliCa 212kbps
+		| DEVICE_TYPE_NFC_18092_424K, // FeliCa 424kbps
+		3000,                         // タイムアウト ms
+		&pInfo
+	);
+	if(!bSuccess){
+		ret << "access failure\r\n";
+		ret << "Error: " << GetFelicaErrorInfo() << "\r\n";
+		return ret.str();
+	}
+
+	// IDm の表示（システム 0）
+	DEVICE_DATA_NFC_18092_212_424K* pdevf = NULL;
+	pdevf = (DEVICE_DATA_NFC_18092_212_424K*)pInfo->dev_info;
+	ret << "IDm = ";
+	for (int i = 0; i < 8; i++)
+	{
+		char buf[32];
+		sprintf(buf, "%02x ", pdevf->NFCID2[i]);
+		ret << buf;
+	}
+	ret << "\r\n";
+
+	// 結果
+	return ret.str();
+}
+#else
+#pragma comment(lib ,"felica.lib")
+#include "felica.h"
+
+std::wstring GetFelicaInfo()
+{
+	std::wostringstream ret;
+	bool b = false;
+
+	// 初期化
+	ret << "initialize...\r\n";
+	b = initialize_library();
+	if(!b){
+		ret << "failed.\r\n";
+		return ret.str();
+	}
+
+	// オープン
+	ret << "open...\r\n";
+	b = open_reader_writer_auto();
+	if(!b){
+		dispose_library();
+		ret << "failed.\r\n";
+		return ret.str();
+	}
+
+	// ポーリング
+	ret << "polling...\r\n";
+	unsigned char system_code[2] = {0xff, 0xff};
+	structure_polling polling;
+	{
+		polling.system_code = system_code;
+		polling.time_slot = 0x00;
+	}
+	unsigned char number_of_cards = 0;
+	structure_card_information card_information;
+	unsigned char card_idm[8] = {0}; 
+	unsigned char card_pmm[8] = {0}; 
+	card_information.card_idm = card_idm;
+	card_information.card_pmm = card_pmm;
+	b = ::polling_and_get_card_information(&polling, &number_of_cards, &card_information);
+	if(!b){
+		close_reader_writer();
+		dispose_library();
+		ret << "failed.\r\n";
+		return ret.str();
+	}
+
+	// 結果
+	ret << "IDm = ";
+	for(int i = 0; i < 8; i++){
+		char buf[32];
+		sprintf(buf, "%02X ", card_idm[i]);
+		ret << buf;
+	}
+	ret << "\r\n";
+
+	// 結果
+	ret << "PMm = ";
+	for(int i = 0; i < 8; i++){
+		char buf[32];
+		sprintf(buf, "%02X ", card_pmm[i]);
+		ret << buf;
+	}
+	ret << "\r\n";
+
+	// 後始末
+	close_reader_writer();
+	dispose_library();
+
+	// 結果
+	return ret.str();
+}
+
+#endif
 
 void CFelicaReaderDlg::OnBnClickedButton1()
 {
-	// FeliCa 情報を読んでみる
-	m_edtInfo.SetWindowText(L"aaa");
+	// FeliCa 情報を取得し、結果を表示
+	m_edtInfo.SetWindowText(GetFelicaInfo().c_str());
 }
